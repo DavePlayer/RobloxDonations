0d50e6581f29b5ed9ce4a6d75a0368bc3cd0b2b6
Kira Yoshikage <dave@pasokon-wa.localhost>
2022-02-28 18:05:34 +0100

    initialized express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req: express.Request, res:express.Response) => {
+    res.send('sedn dunes')
+})
+
+app.listen(9000, () => console.log(`listening on port 9000`))
+
 console.log('works again')
