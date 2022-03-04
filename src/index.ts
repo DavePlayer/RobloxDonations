f7a13ae0b7a3167be6eead118912f99123c54643
Kira Yoshikage <dave@pasokon-wa.localhost>
2022-03-04 23:26:53 +0100

    added google sheets loging

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,93 @@
 import express from 'express'
 import rateLimit from 'express-rate-limit'
 import helmet from 'helmet'
+import SocketIO from 'socket.io'
+import dotenv from 'dotenv'
+import { google } from 'googleapis'
+import socket from 'socket.io'
 
+
+// .env
+dotenv.config()
+
+// server www
 const app = express()
-app.use(express.json({limit: '10kb'}))
+app.use(express.json({ limit: '10kb' }))
 const packetLimit = rateLimit({
     max: 10,// max requests
     windowMs: 60 * 1000, // 1 min
-    message: 'Too many requests' // message to send
+    message: { status: 'error', details: 'too many request' } // message to send
 })
 app.use('/announceDonation', packetLimit)
 app.use(helmet())
 
-app.get('/', (req: express.Request, res:express.Response) => {
+// websockets
+const http = require('http').createServer(app);
+const wws = new socket.Server(http);
+
+wws.on("connection", (socket: SocketIO.Socket) => {
+    console.log("new client connected");
+    socket.send("connection confirmed");
+
+    socket.on("donate::donate", (e, data) => {
+        console.log('emited donate: ', data)
+    });
+});
+
+
+app.get('/', (req: express.Request, res: express.Response) => {
     res.send('sedn dunes')
 })
 
-app.post('/announceDonation', (req: express.Request, res:express.Response) => {
-    console.log(req.body)
-    res.send({xd: 'xd'})
+
+
+interface request {
+    donateImageUrl: string;
+    userName: string;
+    robuxAmmount: number;
+    message: string;
+}
+
+app.post('/announceDonation', async (req: express.Request, res: express.Response) => {
+    if (!req.body) return res.status(406).json({ status: 'error', details: 'no body inside http request' })
+    if (req.body.login != process.env.GAME_LOGIN || req.body.password != process.env.GAME_PASSWORD) return res.status(403).json({ status: 'error', details: 'invalid login data' })
+    const data: request = {
+        donateImageUrl: req.body.donateImageUrl,
+        userName: req.body.userName,
+        robuxAmmount: req.body.robuxAmmount,
+        message: req.body.message,
+    }
+    if (data.donateImageUrl == undefined || data.message == undefined || data.robuxAmmount == undefined || data.userName == undefined) return res.status(406).json({ status: 'error', details: 'invalid http body structure' })
+    const auth = new google.auth.GoogleAuth({
+        scopes: ['https://www.googleapis.com/auth/spreadsheets']
+    });
+    try {
+        const client = await auth.getClient();
+        const sheets = await google.sheets({ version: 'v4', auth })
+        // const response = await sheets.spreadsheets.values.get({ range: `Donations!A2:C2`, spreadsheetId: process.env.SHEET_ID })
+
+        // const [title, data]: any = response.data.values
+
+        // console.log('title: ', title, '\n\ndata: ', data)
+        await sheets.spreadsheets.values.append({
+            spreadsheetId: process.env.SHEET_ID,
+            auth,
+            valueInputOption: 'RAW',
+            insertDataOption: 'INSERT_ROWS',
+            range: `Donations!A:E`,
+            // resource: { values: ['ktoś', 'mihalx', '120', '1970', 'image url', "message"] }
+            requestBody: { values: [[data.userName, 'mihalx', data.robuxAmmount, Date.now(), data.donateImageUrl, data.message]] }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+    wws.emit('donate::donate', data)
+
+    res.send({ xd: 'xd' })
 })
 
-app.listen(9000, () => console.log(`listening on port 9000`))
+app.listen(9000, () => console.log(`www on port 9000`))
+http.listen(8080, () => console.log("http working on 8080"));
 
 console.log('works again')
